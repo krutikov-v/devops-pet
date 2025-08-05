af96676b77880ae4da4260a0400c5ee0965dc442
Vitalii <krutikov.vitaly@gmail.com>
2025-08-05 16:58:35 +0200

    настройка grafana и prometheus в docker-compose.yml

--- nasa-project/backend/app.py
+++ nasa-project/backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
-
+from prometheus_flask_exporter import PrometheusMetrics
+ 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
 
 @app.route("/api/health")
 def health():
